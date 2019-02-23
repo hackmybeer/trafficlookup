447c1b8af48ba3d650777dbd49d2ab3708c71d0a
Silas <Silas.Schimmel@gmx.de>
2019-02-23 19:51:39 +0100

    little changes

--- popup/sysinfo.js
+++ popup/sysinfo.js
@@ -172,13 +172,13 @@ var e = module.init(),
 
 debug = '';
 
-debug += '<tr> <td class="left"> Betriebsystem </td> <td class="right">' + e.os.name + '</td><tr/>';
-debug += '<tr> <td class="left"> Version </td> <td class="right">' + e.os.version + '</td><tr/>';
-debug += '<tr> <td class="left"> Browser </td> <td class="right">' + e.browser.name + '</td><tr/>';
-debug += '<tr><td class="left"> Browserversion </td> <td class="right">' + e.browser.version + '</td><tr/>';
-debug += '<tr><td class="left"> UserAgent </td> <td class="right">' + navigator.userAgent + '</td><tr/>';
-debug += '<tr><td class="left"> AppVersion> </td> <td class="right">' + navigator.appVersion + '</td><tr/>';
-debug += '<tr><td class="left"> Platform  </td><td class="right">' + navigator.platform + '</td><tr/>';
+debug += '<tr> <td class="left"> Betriebsystem: </td> <td class="right">' + e.os.name + '</td><tr/>';
+debug += '<tr> <td class="left"> Version: </td> <td class="right">' + e.os.version + '</td><tr/>';
+debug += '<tr> <td class="left"> Browser: </td> <td class="right">' + e.browser.name + '</td><tr/>';
+debug += '<tr><td class="left"> Browserversion: </td> <td class="right">' + e.browser.version + '</td><tr/>';
+debug += '<tr><td class="left"> UserAgent: </td> <td class="right">' + navigator.userAgent + '</td><tr/>';
+debug += '<tr><td class="left"> AppVersion: </td> <td class="right">' + navigator.appVersion + '</td><tr/>';
+debug += '<tr><td class="left"> Platform:  </td><td class="right">' + navigator.platform + '</td><tr/>';
 
 browser.browsingData.settings()
 .then(function(result){
