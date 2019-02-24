e97a5a20e240fa9aed11fecee9ff1448a1aa6b3a
Maris <maris.beer@icloud.com>
2019-02-24 11:05:50 +0100

    choose_beast.js deleted lines 94-96

--- popup/choose_beast.js
+++ popup/choose_beast.js
@@ -87,12 +87,3 @@ function reportExecuteScriptError(error) {
   document.querySelector("#error-content").classList.remove("hidden");
   console.error(`Failed to execute beastify content script: ${error.message}`);
 }
-
-/**
- * When the popup loads, inject a content script into the active tab,
- * and add a click handler.
- * If we couldn't inject the script, handle the error.
- */
-browser.tabs.executeScript({file: "/content_scripts/beastify.js"})
-.then(listenForClicks)
-.catch(reportExecuteScriptError);
