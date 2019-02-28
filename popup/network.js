e54d6dc641d1661d746a8479fb8d2366811559a6
Sascha <sascha@agroeschl.de>
2019-03-01 00:26:17 +0100

    first work with network and java script

--- popup/network.js
+++ popup/network.js
@@ -1,9 +1,29 @@
-async function logRequests() {
-  let harLog = await browser.devtools.network.getHAR();
-  console.log(`HAR version: ${harLog.version}`);
-  for (let entry of harLog.entries) {
-    console.log(entry.request.url);
-  }
-}
+console.log("network.js loaded!");
 
-logRequestsButton.addEventListener("click", logRequests);
+var nURL = window.location.href;
+console.log("nURL_1 =" + nURL);
+/*
+function httpGet(nURL)
+{
+    if (window.XMLHttpRequest)
+    {   //for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {   //for IE6, IE5 -> Microsoft sucks -> GitHub nooo why.
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", nURL, false );
+    xmlhttp.send();
+
+    console.log("nURL =" + nURL);
+    console.log("xmlhttp.send() : + "xmlhttp.send());
+}
+*/
